103d2e92e0c0794f3d59be8cebd8de9c457efc95
Sandhya Konjarla <reachout.sandhya@gmail.com>
2025-03-10 20:58:27 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-apt-get update && apt-get install -y tesseract-ocr
+apt-get update && apt-get install -y tesseract-ocr tesseract-ocr-eng tesseract-ocr-osd
+export TESSDATA_PREFIX="/usr/share/tesseract-ocr/4.00/tessdata/"
